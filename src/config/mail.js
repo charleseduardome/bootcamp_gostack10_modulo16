4a420406671786b056afcf2b000ef5253fb55882
charleseduardome <charleseduardo.me@gmail.com>
2020-03-06 11:42:55 -0300

    Configurando NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9d46f8fcc6512d',
+    pass: 'ae654d86c3e3f3',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
