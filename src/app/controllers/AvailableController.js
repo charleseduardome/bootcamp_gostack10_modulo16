9c185d95e0d9760fade4383b6b312cb99022c3bb
charleseduardome <charleseduardo.me@gmail.com>
2020-03-24 22:48:29 -0300

    Correção nome da variável

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -46,7 +46,7 @@ class AvailableController {
       '20:00',
     ];
 
-    const avaiable = schedule.map(time => {
+    const available = schedule.map(time => {
       const [hour, minute] = time.split(':');
 
       const value = setSeconds(
@@ -56,13 +56,13 @@ class AvailableController {
       return {
         time,
         value: format(value, "yyy-MM-dd'T'HH:mm:ssxxx"),
-        avaiable:
+        available:
           isAfter(value, new Date()) &&
           !appointments.find(a => format(a.date, 'HH:mm') === time),
       };
     });
 
-    return res.json(avaiable);
+    return res.json(available);
   }
 }
 
