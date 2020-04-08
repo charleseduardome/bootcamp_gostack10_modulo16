d5a33106b9774e00222f9624c89593da9bdc6219
charleseduardome <charleseduardo.me@gmail.com>
2020-04-08 09:00:08 -0300

    Trabalhando com cache

--- src/app/services/CreateAppointmentService.js
+++ src/app/services/CreateAppointmentService.js
@@ -5,6 +5,7 @@ import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 import Notification from '../schemas/Notification';
+import Cache from '../../lib/Cache';
 
 class CreateAppointmentService {
   async run({ provider_id, user_id, date }) {
@@ -58,6 +59,11 @@ class CreateAppointmentService {
       content: `Novo agendamento de ${user.name} para o ${formattedDate} `,
       user: provider_id,
     });
+
+    /**
+     * Invalidate cache
+     */
+    await Cache.invalidatePrefix(`user:${user.id}:appointments`);
 
     return appointment;
   }
